718fea0531cc75e3ab86d12b0fae4c31cc50c6a1
Laawe <laura.render.03@gmail.com>
2018-03-01 14:03:26 -0600

    cardify vol 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-Escribe tu código aqui----
+//elementos html
+let button = document.getElementById("btn");
+let gallery = document.getElementById("gallery");
+//
+let images = document.getElementsByClassName("imagen");
+console.log(images);
+
+let arrayImg = Array.from(images);
+console.log(arrayImg);
+
+button.addEventListener("click", figure);
+
+function figure(){
+    arrayImg.forEach((element,index) =>{
+        
+    })
+}
