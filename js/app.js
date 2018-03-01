0a1f9a82fb9da0a4dfde2d9a70bd1fbe4ff084bf
AnnKrn <anakrn.carreon@gmail.com>
2018-03-01 10:18:06 -0600

    completo sin estilo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-Escribe tu código aqui----
+// Escribe tu código aqui----
+
+
+const obteniendoElElemento = document.getElementById("gallery")
+console.log(Array.from(obteniendoElElemento));
+
+let objImg = {
+    'src':'',
+    'alt':''
+};
+console.log(obteniendoElElemento)
+
+const nombreDeTuFuncion = element =>{
+    let arrayImages = Array.from(obteniendoElElemento.getElementsByTagName("img"));
+    console.log(arrayImages)
+
+    // const arrayGallery = Array.from(document.getElementsByTagName('img'))
+
+    arrayImages.forEach(img => {
+    // console.log(img.src)
+        objImg = {
+            'src': img.src,
+            'alt': img.alt
+        };
+        console.log(objImg)
+
+        let figureTemplate = `<figure><img src=${objImg.src}></img><figcaption>${objImg.alt}</figcaption></figure>`;
+        //reemplazar los nodos img por el template armado, usando el atributo outerHTML
+        img.outerHTML = figureTemplate;
+    });
+
+};
+
+nombreDeTuFuncion();
