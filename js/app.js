4a8bf537146d606377e1f006020e44dc877f36d7
Jess Moran <j-es-itza@hotmail.com>
2018-03-01 12:13:43 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-Escribe tu código aqui----
+const imgContainer = document.querySelector("section");
+//console.log(imgContainer);
+
+const getImg = element =>{
+  const images = element.querySelectorAll("img")
+  const imgArr = Array.from(images);
+  newElement (imgArr)
+}
+
+
+const newElement = array =>{
+  let change = array.map(function(item){
+  let newfigcaption = document.createElement("figcaption");
+  newfigcaption.innerText = (item.alt);
+
+  const newFigure = document.createElement("figure");
+  newFigure.appendChild(newfigcaption);
+  newFigure.appendChild(item);
+  imgContainer.appendChild(newFigure);
+
+  })
+
+}
+
+ getImg (imgContainer);
